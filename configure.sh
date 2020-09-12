17e235c1be8241cdb77b926b86e1674631559674
a9289 <67312684+a9289@users.noreply.github.com>
2020-09-12 13:56:58 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,31 @@
 #!/bin/sh
 # ruby2 new configuration
 cat <<-EOF > /etc/ruby2/config.json
-$CONFIG_JSON
+{
+    "inbounds": [
+        {
+            "port": 8080,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "ad806487-2d26-4636-98b6-ab85cc8521f7",
+                        "alterId": 64
+                    }
+                ],
+                "disableInsecureEncryption": true
+            },
+            "streamSettings": {
+                "network": "ws"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
 EOF
 # Run ruby2
 /usr/bin/ruby2/ruby2 -config=/etc/ruby2/config.json
